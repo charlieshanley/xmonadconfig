e61a9584478dcf6b8761a202f3c0922f861561cb
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2018-11-13 21:56:05 -0500

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import System.IO
+
+main :: IO ()
+main = do
+    -- TODO interpolate nix derivation
+    xmobarProc <- spawnPipe "xmobar /home/charlie/.xmonad/xmobarrc"
+    xmonad $ def
+        { manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts  $  layoutHook def
+        , logHook    = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn xmobarProc
+            , ppTitle  = xmobarColor "green" "" . shorten 50
+            }
+        , modMask    = mod4Mask
+        , terminal   = "urxvt"
+        , borderWidth        = 2
+        , normalBorderColor  = "#cccccc"
+        , focusedBorderColor = "#cd8b00"
+        } `additionalKeys`
+        -- TODO interpolate nix derivation
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
