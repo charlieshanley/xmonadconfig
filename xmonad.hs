2dbc8ee6521653f318b9c021c99e9f9e1a7a2abb
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2018-11-18 17:16:06 -0500

    space between windows

--- xmonad.hs
+++ xmonad.hs
@@ -3,26 +3,30 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders (smartBorders)
 import System.IO
 
 main :: IO ()
 main = do
     -- TODO interpolate nix derivation
-    xmobarProc <- spawnPipe "xmobar /home/charlie/.xmonad/xmobarrc"
     spawn "feh --bg-center /home/charlie/Downloads/footerBG.jpg"
+    xmobarProc <- spawnPipe "xmobar /home/charlie/.xmonad/xmobarrc"
     xmonad $ docks def
         { manageHook = manageDocks <+> manageHook def
-        , layoutHook = smartBorders . avoidStruts  $  layoutHook def
+        , layoutHook =
+            spacingRaw True (Border 0 0 0 0) False (Border 5 5 5 5) True .
+            smartBorders .
+            avoidStruts . 
+            layoutHook $ def
         , logHook    = dynamicLogWithPP $ xmobarPP
             { ppOutput = hPutStrLn xmobarProc
-            -- , ppTitle  = xmobarColor "green" "" . shorten 50
             , ppTitle = (>> "")
             }
         , modMask    = mod4Mask
         , terminal   = "urxvt"
         , borderWidth        = 2
-        , normalBorderColor  = "#cccccc"
+        -- , normalBorderColor  = "#cccccc"
         , focusedBorderColor = "#cd8b00"
         } `additionalKeys`
         -- TODO interpolate nix derivation
